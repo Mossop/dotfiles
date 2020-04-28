f6911f1a1883b0b8edd601dd2c5665820270694c
Dave Townsend <dtownsend@oxymoronical.com>
2020-04-28 13:05:34 -0700

    Only load config when needed.

--- includes/config.sh
+++ includes/config.sh
@@ -3,9 +3,11 @@ export_config() {
   export DOTFILES_BRANCH
 }
 
-if [ -f "$DOTFILES/external/.config" ]; then
-  . "$DOTFILES/external/.config"
-else
-  echo "Dotfiles is not correctly installed. No .config file exists."
-  exit 1
-fi
+load_config() {
+  if [ -f "$DOTFILES/external/.config" ]; then
+    . "$DOTFILES/external/.config"
+  else
+    echo "Dotfiles is not correctly installed. No .config file exists."
+    exit 1
+  fi
+}
