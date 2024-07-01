17ba589152e83bb97a1aee5efd10a7358e5f72c4
Dave Townsend <dtownsend@oxymoronical.com>
2024-07-01 10:29:00 +0100

    Add mise startup

--- platforms/linux/startup.sh
+++ platforms/linux/startup.sh
@@ -1,3 +1,7 @@
 if [ -x "$HOME/mozilla/bin/startup" ]; then
   alias mozb="$HOME/mozilla/bin/startup"
 fi
+
+if [ -f "$HOME/.local/bin/mise" ]; then
+  eval "$HOME/.local/bin/mise activate bash)"
+fi
