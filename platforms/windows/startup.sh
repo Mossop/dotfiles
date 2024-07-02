55dbbb4aca859efd29806123c466ee7711e0927e
Dave Townsend <dtownsend@oxymoronical.com>
2024-07-02 14:45:11 +0100

    Support mozilla build on a dev drive

--- platforms/windows/startup.sh
+++ platforms/windows/startup.sh
@@ -2,6 +2,8 @@ if [ -d "/c/Users/Dave/AppData/Local/Programs/Microsoft VS Code" ]; then
   alias code=vscode
 fi
 
-if [ -x "/c/mozilla/bin/startup" ]; then
+if [ -x "/e/mozilla/bin/startup" ]; then
+  alias mozb="/e/mozilla/bin/startup"
+elif [ -x "/c/mozilla/bin/startup" ]; then
   alias mozb="/c/mozilla/bin/startup"
 fi
