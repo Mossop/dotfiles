15c44a74658443ec8c66d8f8eea7792d9f53064a
Dave Townsend <dtownsend@oxymoronical.com>
2024-09-20 15:26:11 +0100

    Enable starship and direnv for windows

--- platforms/windows/startup.sh
+++ platforms/windows/startup.sh
@@ -2,8 +2,18 @@ if [ -d "/c/Users/Dave/AppData/Local/Programs/Microsoft VS Code" ]; then
   alias code=vscode
 fi
 
-if [ -f "/e/mozilla/bin/activate" ]; then
-  export MOZBUILD="/e/mozilla/bin/activate"
-elif [ -f "/c/mozilla/bin/activate" ]; then
-  export MOZBUILD="/c/mozilla/bin/activate"
+if [ -f "$HOME/bin/direnv.exe" ]; then
+  export _unmangle_direnv_names="PATH"
+  _unmangle_direnv_paths() {
+      for k in $_unmangle_direnv_names; do
+          eval "$k=\"\$(/usr/bin/cygpath -p \"\$$k\")\""
+      done
+  }
+  eval "$(direnv hook bash | sed -e 's@export bash)@export bash)\
+    _unmangle_direnv_paths@')"
+fi
+
+if [ -x "$HOME/.cargo/bin/starship.exe" ]; then
+  export STARSHIP_CONFIG=$DOTFILES/shared/starship.toml
+  eval "$($HOME/.cargo/bin/starship.exe init bash)"
 fi
