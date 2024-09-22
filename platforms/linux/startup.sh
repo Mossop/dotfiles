eab4e355a1e586ac259fe75618d7b42771a00bc5
Dave Townsend <dtownsend@oxymoronical.com>
2024-09-22 14:40:39 +0100

    Activate starship on linux

--- platforms/linux/startup.sh
+++ platforms/linux/startup.sh
@@ -5,3 +5,8 @@ fi
 if [ -f "$HOME/.local/bin/mise" ]; then
   eval "$($HOME/.local/bin/mise activate bash)"
 fi
+
+if [ -x "/usr/local/bin/starship" ]; then
+  export STARSHIP_CONFIG=$DOTFILES/shared/starship.toml
+  eval "$(/usr/local/bin/starship init bash)"
+fi
