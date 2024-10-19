f2f72b1b287f2db60abfed7b97f9c35e1084d9dd
Dave Townsend <dtownsend@oxymoronical.com>
2024-10-19 19:11:35 +0100

    Use mingw git

--- platforms/windows/startup.sh
+++ platforms/windows/startup.sh
@@ -2,6 +2,8 @@ if [ -d "/c/Users/Dave/AppData/Local/Programs/Microsoft VS Code" ]; then
   alias code=vscode
 fi
 
+export PATH=$(echo $PATH | sed -e s~/Git/cmd~/Git/mingw64/bin~g)
+
 if [ -f "$HOME/bin/direnv.exe" ]; then
   export _unmangle_direnv_names="PATH"
   _unmangle_direnv_paths() {
