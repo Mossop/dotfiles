0fb9e540a3b18ef1a3cdda580e56bc3d711277b4
Dave Townsend <dtownsend@oxymoronical.com>
2025-10-02 10:46:22 +0100

    Update mise when installing

--- platforms/macos/install.sh
+++ platforms/macos/install.sh
@@ -1,4 +1,4 @@
 if command -v brew 1>/dev/null 2>&1; then
-  brew upgrade
-  brew install git joe gnu-which findutils gnu-sed gnu-tar grep coreutils wget curl starship mise
+  brew upgrade --quiet
+  brew install --quiet git joe gnu-which findutils gnu-sed gnu-tar grep coreutils wget curl starship mise
 fi
